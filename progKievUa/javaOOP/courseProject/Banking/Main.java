8e9dbf5381f530d572d4fd165fd937c69532fa1f
Alexander <alexander_kohan@outlook.com>
2016-08-01 18:14:43 +0300

    project

---
+++ progKievUa/javaOOP/courseProject/Banking/Main.java
@@ -0,0 +1,13 @@
+package progKievUa.javaOOP.courseProject.Banking;
+
+/**
+ * Created by Don on 31.07.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Currency USA = new Currency("Dollar",0.2);
+
+        System.out.println(USA);
+    }
+}
---
+++ progKievUa/javaOOP/courseProject/Banking/Purce.java
@@ -0,0 +1,7 @@
+package progKievUa.javaOOP.courseProject.Banking;
+
+/**
+ * Created by Don on 31.07.2016.
+ */
+public class Purce {
+}
