02548bb9d17de309c8c236a347961a2080654d71
Alexander Kokhan <alexander_kohan@outlook.com>
2016-08-17 16:47:33 +0300

    class code 9 lesson

---
+++ progKievUa/javaOOP/classWork/lesson8/collections/_4_set/_2_hash_set/_2_IncorrectElemSample.java
@@ -0,0 +1,30 @@
+package progKievUa.javaOOP.classWork.lesson8.collections._4_set._2_hash_set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Bohdan Vanchuhov
+ */
+public class _2_IncorrectElemSample {
+    public static void main(String[] args) {
+        Set<A> set = new HashSet<>();
+        set.add(new A(10));
+        set.add(new A(10));
+
+        System.out.println(set);
+    }
+}
+
+class A {
+    private int x;
+
+    public A(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(x);
+    }
+}
