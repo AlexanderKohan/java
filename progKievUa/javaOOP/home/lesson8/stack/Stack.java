cf5f04f6b7ac2659857e4408e78249843570996d
Alexander Kokhan <alexander_kohan@outlook.com>
2016-08-20 22:05:41 +0300

    class code

---
+++ progKievUa/javaOOP/home/lesson8/stack/Stack.java
@@ -0,0 +1,47 @@
+package progKievUa.javaOOP.home.lesson8.stack;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Created by Alexander on 8/20/2016.
+ */
+public interface Stack<E> extends Iterable<E>  {
+    /**
+     * Pushes an element onto the stack
+     *
+     * @param elem the element to push
+     */
+    void push(E elem);
+
+    /**
+     * Retrieves and removes the first element of this stack
+     *
+     * @return the head of this stack
+     * @throws NoSuchElementException if this stack is empty
+     */
+    E pop();
+
+    /**
+     * Retrieves, but does not remove, the first element of this stack
+     *
+     * @return the head of this stack
+     * @throws NoSuchElementException if this stack is empty
+     */
+    E peek();
+
+    /**
+     * Returns the number of elements in this stack.  If this stack contains
+     * more than <tt>Integer.MAX_VALUE</tt> elements, returns
+     * <tt>Integer.MAX_VALUE</tt>.
+     *
+     * @return the number of elements in this stack
+     */
+    int size();
+
+    /**
+     * Returns <tt>true</tt> if this stack contains no elements.
+     *
+     * @return <tt>true</tt> if this stack contains no elements
+     */
+    boolean isEmpty();
+}
